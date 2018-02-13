8022a127702ee870a7f12a46d7d524d9d612192d
jade alvares <j.alvares@nhm.ac.uk>
2018-02-13 13:04:24 +0000

    added two methods to add and search names and exported them

--- index.js
+++ index.js
@@ -7,3 +7,32 @@ mongoose.Promise = global.Promise;
 const db = mongoose.connect('mongodb://localhost:27017/customercli', {
   useMongoClient: true
 });
+
+// Import model
+const Customer = require('./models/customer');
+
+// Add customer
+const addCustomer = (customer) => {
+  Customer.create(customer).then(customer => { //callbacks with es6 and promise
+    console.info('New Customer added');
+    db.close(); //closes the db
+  });
+}
+
+// Find customer
+const findCustomer = (name) => {
+  // Make case insensitive so we can find any case
+  const search = new RegExp(name, 'i'); //lowecase i for case insensitive
+  Customer.find({$or: [{firstname: search}, {lastname: search}]}) //Customer is the customer model
+    .then(customer => {
+      console.info(customer);
+      console.info(`${customer.length} matches`); //back ticks allow interpolation
+      db.close();
+    });
+}
+
+//Export all methods
+module.exports = {
+  addCustomer,
+  findCustomer
+}
