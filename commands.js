1d144a9be944d867bb1f012d0e0da1ec2f0d34c6
jade alvares <j.alvares@nhm.ac.uk>
2018-02-13 13:15:35 +0000

    writing commands for terminal with commander.js

--- commands.js
+++ commands.js
@@ -8,4 +8,19 @@ program
   .version('1.0.0')
   .description('Client Management System')
 
-  program.parse(process.argv);
+program
+  .command('add <firstname> <lastname> <phone> <email>')
+  .alias('a')
+  .description('Add a customer')
+  .action((firstname, lastname, phone, email) => {
+    addCustomer({firstname, lastname, phone, email});
+  });
+
+program
+  .command('find <name>')
+  .alias('f')
+  .description('Find a customer')
+  .action(name => findCustomer(name));
+
+
+program.parse(process.argv);
