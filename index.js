753437202d5385a45e08008ba5d0550a974f48c4
jade alvares <j.alvares@nhm.ac.uk>
2018-02-13 13:10:32 +0000

    adding commands using commander.js

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const mongoose = require('mongoose');
 //mongo promises are deprecated so need to map global promise
 mongoose.Promise = global.Promise;
 //connect to DB
-const db = mongoose.connect('mongodb://localhost:27017/customercli', {
-  useMongoClient: true
-});
+const db = mongoose.connect('mongodb://localhost:27017/customercli');
 
 // Import model
 const Customer = require('./models/customer');
--- models/customer.js
+++ models/customer.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 //customer schema
 const customerSchema = mongoose.Schema({
   firstname: { type: String },
-  lastname: { type: String };
+  lastname: { type: String },
   phone: { type: String },
   email: { type: String }
 });
