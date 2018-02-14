0cb2062afd8b4a14fc2c83da5293677ed1c6d4f1
jjade seravla <jadealvares@hotmail.co.uk>
2018-02-14 09:39:38 +0000

    prompts user for inidividual input

--- commands.js
+++ commands.js
@@ -1,20 +1,53 @@
 const program = require('commander');
+const { prompt } = require('inquirer');
 const {
   addCustomer,
   findCustomer
 } = require('./index');
 
+//Customer questions
+const questions = [
+  {
+    type: 'input',
+    name: 'firstname',
+    message: 'Customer first name'
+  },
+  {
+    type: 'input',
+    name: 'lastname',
+    message: 'Customer last name'
+  },
+  {
+    type: 'input',
+    name: 'phone',
+    message: 'Customer phone number'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Customer email address'
+  }
+];
+
 program
   .version('1.0.0')
   .description('Client Management System')
 
+// program
+//   .command('add <firstname> <lastname> <phone> <email>')
+//   .alias('a')
+//   .description('Add a customer')
+//   .action((firstname, lastname, phone, email) => {
+//     addCustomer({firstname, lastname, phone, email});
+//   });
+
 program
-  .command('add <firstname> <lastname> <phone> <email>')
+  .command('add')
   .alias('a')
   .description('Add a customer')
-  .action((firstname, lastname, phone, email) => {
-    addCustomer({firstname, lastname, phone, email});
-  });
+  .action(() => {
+    prompt(questions).then(answers => addCustomer(answers)); //gives a promise which then returns you answers
+  })
 
 program
   .command('find <name>')
